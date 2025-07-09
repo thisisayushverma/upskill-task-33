406cd7b3f79cead82f687b0be31278c891cec121
Ayush Verma <devilofilluminati@gmail.com>
2025-07-09 23:30:23 +0530

    backend changes

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://upskill-task-33.vercel.app',
   credentials: true
 }));
 app.use(express.json());
