2e7cb6c750bbf7518203eb5a36d86064b5be2dd6
Ayush Verma <devilofilluminati@gmail.com>
2025-07-09 23:25:11 +0530

    backend changes

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,12 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
